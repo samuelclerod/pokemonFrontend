e3cc4396ef0002ca935bbf29bc1b68a169a3f664
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-01-21 17:57:40 -0300

    example

--- src/components/Listagem/index.js
+++ src/components/Listagem/index.js
@@ -1,44 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import ListagemStyle from './styles';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import ListagemStyle from "./styles";
+import axios from "axios";
 
 const Listagem = () => {
-    const [repository, setRepository] =  useState([]);
-    useEffect(() => {
-        loadPokemon();
-    }, []);
+  const [repository, setRepository] = useState([]);
+  useEffect(() => {
+    async function loadPokemons() {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=5&"
+      );
 
-    async function loadPokemon () {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0');
-        const repositoryData = response.data.results;
-        
-        repositoryData.map(resposta => {
-            const arrayResposta = resposta.url;
-            
-            async function newPokemon() {
-                const novoaxios = await axios.get(arrayResposta)
-                console.log(novoaxios.data);
-            }
+      const repositoryData = response.data.results;
 
-            newPokemon();
-              
-        });
-        //setRepository();
-    };
+      const arrayOfPromises = repositoryData.map((eachResult) => {
+        return axios.get(eachResult.url);
+      });
 
-    return (
+      //   Aqui tem um array de resultados do fetch do axios. Precisa iterar sobre eles e pegar o .data
+      // Pq é dentro do .data que tão os pokemons
+      const arrayOfAxiosResults = await Promise.all(arrayOfPromises);
+
+      console.log(arrayOfAxiosResults);
+
+      const pokemonsData = arrayOfAxiosResults.map((response) => response.data);
+
+      setRepository(pokemonsData);
+    }
+
+    loadPokemons();
+  }, []);
+
+  return (
     <>
-        <ListagemStyle><h1>Hello World</h1></ListagemStyle>
-        {repository.map(pokemon => (
-            <>
-                <ul key={pokemon.name}>
-                    <li>{pokemon.name}</li>
-                    <li>{pokemon.url}</li>
-                </ul>           
-            </>    
-        ))}
+      <ListagemStyle>
+        <h1>Hello World</h1>
+      </ListagemStyle>
+      {repository.map((pokemon) => (
+        <>
+          <ul key={pokemon.name}>
+            <li>{pokemon.name}</li>
+            <li>{pokemon.url}</li>
+          </ul>
+        </>
+      ))}
     </>
-    )
+  );
 };
 
 export default Listagem;
